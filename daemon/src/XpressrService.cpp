983dcf3188fa2a09adc626db36ea874b2a6e2f76
JeysonFlores <jeyson.flores@protonmail.com>
2021-12-23 18:40:37 -0700

    main function improved

--- daemon/src/XpressrService.cpp
+++ daemon/src/XpressrService.cpp
@@ -1,22 +1,20 @@
 #include "Interfaces/RegexInterface.h"
 #include "Utils/Logger.h"
 #include <iostream>
+#include <cstdlib>
 
 INIT_LOGGER;
 
 // TODO: Put the session's creation inside a Try-Catch statement
 int main(int /*argc*/, char* /*argv*/[])
 {
-    LOG(INFO, "Service Started");
-
-    const char* serviceName = "com.github.jeysonflores.xpressrService";
-    auto connection = sdbus::createSessionBusConnection(serviceName);
-
+    auto connection = sdbus::createSessionBusConnection("com.github.jeysonflores.xpressrService");
     LOG(SUCCESS, "Name requested successfully...");
 
-    const char* objectPath = "/com/github/jeysonflores/xpressrService/Regex";
-    RegexInterface server(*connection, objectPath);
+    RegexInterface server(*connection, "/com/github/jeysonflores/xpressrService/Regex");
 
+    LOG(INFO, "Service Starting...");
     connection->enterEventLoop();
+
     LOG(INFO, "Service ended");
 }
