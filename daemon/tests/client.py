c9e6489ad6d85285b7e101a689d26b62ebf38e45
JeysonFlores <jeyson.flores@protonmail.com>
2021-12-28 16:31:59 -0700

    client done

--- daemon/tests/client.py
+++ daemon/tests/client.py
@@ -2,5 +2,23 @@ import gi
 gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
+import dbus
 
-#TODO: Client stuff
+def main():
+    bus = dbus.SessionBus()
+
+    remote_object = bus.get_object("com.github.jeysonflores.xpressrService", "/com/github/jeysonflores/xpressrService/Regex")
+
+    iface = dbus.Interface(remote_object, "com.github.jeysonflores.Regex")
+
+    regexes = iface.GetRegexes()
+
+    for regex in regexes:
+        print("Id: " + str(regex[0]))
+        print("Name: " + str(regex[1]))
+        print("Regex: " + str(regex[2]))
+        print("Example: " + str(regex[3]))
+        print("\n")
+
+if __name__ == '__main__':
+    main()
