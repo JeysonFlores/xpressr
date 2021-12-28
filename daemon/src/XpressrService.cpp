6228ef0e22b83eed4b468cc12bfc83fc7ad530c0
JeysonFlores <jeyson.flores@protonmail.com>
2021-12-28 14:59:14 -0700

    Local database path configured

--- daemon/src/XpressrService.cpp
+++ daemon/src/XpressrService.cpp
@@ -3,17 +3,31 @@
 #include "Utils/Logger.h"
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
+#include <string>
 #include <tuple>
 #include <type_traits>
 
 INIT_LOGGER;
 
-// TODO: Put the session's creation inside a Try-Catch statement
 int main(int /*argc*/, char* /*argv*/[])
 {
+
     auto connection = sdbus::createSessionBusConnection("com.github.jeysonflores.xpressrService");
+
     LOG(SUCCESS, "Name requested successfully...");
-    XpressrService::Services::DataManager dataManager("dbfile.db");
+
+    std::string env_p;
+    if (std::getenv("XPRESSR_CONFIG_FLATPAK"))
+        env_p = std::getenv("XPRESSR_CONFIG_FLATPAK");
+    else {
+        std::string db_path = "/.local/share/com.github.jeysonflores.xpressr.db";
+        env_p = std::getenv("HOME") + db_path;
+    }
+
+    std::cout << "Database path is: " << env_p << '\n';
+
+    XpressrService::Services::DataManager dataManager(env_p.c_str());
 
     XpressrService::Interfaces::Regex server(*connection, "/com/github/jeysonflores/xpressrService/Regex", dataManager);
 
